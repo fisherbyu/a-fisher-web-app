5cb6245a8cb6b99f697e23fe536347355554f4d2
fisherbyu <fisherandrew777@gmail.com>
2024-03-04 09:28:23 -0700

    Update Constructors to Map DB to DTO

--- Models/Album.cs
+++ Models/Album.cs
@@ -18,6 +18,7 @@ namespace AFisherWebApp.Models
         public Image Image { get; set; }
     }
 
+    
     public class AlbumDto
     {
         public int rank { get; set; }
@@ -26,6 +27,29 @@ namespace AFisherWebApp.Models
         public List<TagDto> tags { get; set; }
         public LinkDto link { get; set; }
         public ImageDto image { get; set; }
+
+        // Constructor to Map Album to AlbumDto for Export
+        public AlbumDto(Album album)
+        {
+            rank = album.Rank;
+            name = album.Name;
+            content = new List<string>();
+            tags = new List<TagDto>();
+            link = new LinkDto { appleURI = album.Link.AppleURI, spotifyURI = album.Link.SpotifyURI };
+            image = new ImageDto { src = album.Image.Src, alt = album.Image.Alt };
+
+            // Extract Lists for Content and Tags
+            foreach (Content c in album.Content)
+            {
+                content.Add(c.Text);
+            }
+
+            foreach (Tag t in album.Tags)
+            {
+                tags.Add(new TagDto { title = t.Title, content = t.Content });
+            }
+
+        }
     }
 }
 
--- Models/Artist.cs
+++ Models/Artist.cs
@@ -24,12 +24,37 @@ namespace AFisherWebApp.Models
     public class ArtistDto
     {
         public int tier { get; set; }
-        public int rank { get; set; }
+        public int? rank { get; set; }
         public string name { get; set; }
         public List<string> content { get; set; }
         public List<TagDto> tags { get; set; }
         public LinkDto link { get; set; }
         public ImageDto image { get; set; }
+
+        // Constructor to Map Artist to ArtistDto for Export
+        public ArtistDto(Artist artist)
+        {
+            tier = artist.Tier;
+            rank = artist.Rank;
+            name = artist.Name;
+            content = new List<string>();
+            tags = new List<TagDto>();
+            link = new LinkDto { appleURI = artist.Link.AppleURI, spotifyURI = artist.Link.SpotifyURI };
+            image = new ImageDto { src = artist.Image.Src, alt = artist.Image.Alt };
+
+            // Extract Lists for Content and Tags
+            foreach(Content c in artist.Content)
+            {
+                content.Add(c.Text);
+            }
+
+            foreach (Tag t in artist.Tags)
+            {
+                tags.Add(new TagDto { title = t.Title, content = t.Content });
+            }
+        }
     }
+
+
 }
 
