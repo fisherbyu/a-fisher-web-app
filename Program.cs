4b46edeb75edda9f25b1848e13f5bf9709533fa3
fisherbyu <fisherandrew777@gmail.com>
2024-03-04 14:16:36 -0700

    Publish Site

--- Program.cs
+++ Program.cs
@@ -25,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(p => p.WithOrigins("http://localhost:3000"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
