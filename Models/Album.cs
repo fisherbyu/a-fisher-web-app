be1fc0a0d51fced3dc3cddbc02f1096a961eab05
fisherbyu <fisherandrew777@gmail.com>
2024-04-26 00:23:54 -0600

    Configure DTO mapping

--- Models/Album.cs
+++ Models/Album.cs
@@ -6,6 +6,7 @@ namespace AFisherWebApp.Models
 {
 	public class Album
 	{
+        // Attributes
         [Key]
         [Required]
         public int Id { get; set; }
@@ -21,12 +22,21 @@ namespace AFisherWebApp.Models
 
     public class AlbumDto
     {
-        // Constructor
+        // Constructors
+
+        // For Creating a Dto from Json Inputs (Default)
+        public AlbumDto()
+        {
+            Type = "Album";
+        }
+
+        // For Creating a Dto from an Artist
         public AlbumDto(Album album)
         {
             Id = album.Id;
             Rank = album.Rank;
             Name = album.Name;
+            Type = "Album";
             Content = album.Content.Select(c => c.Text).ToList();
             Tags = album.Tags.Select(t => new TagDto { Title = t.Title, Content = t.Content }).ToList();
             Link = new LinkDto { AppleURI = album.Link.AppleURI, SpotifyURI = album.Link.SpotifyURI };
@@ -34,7 +44,7 @@ namespace AFisherWebApp.Models
         }
 
         // Attributes
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public int Rank { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
@@ -42,6 +52,21 @@ namespace AFisherWebApp.Models
         public List<TagDto> Tags { get; set; }
         public LinkDto Link { get; set; }
         public ImageDto Image { get; set; }
+
+        // Methods
+        public Album ToAlbum()
+        {
+            return new Album
+            {
+                Id = this.Id ?? 0,
+                Rank = this.Rank,
+                Name = this.Name,
+                Content = this.Content.Select((text, index) => new Content { Text = text, Order =  index}).ToList(),
+                Tags = this.Tags.Select(tagDto => new Tag { Title = tagDto.Title, Content = tagDto.Content }).ToList(),
+                Link = new Link { AppleURI = this.Link.AppleURI, SpotifyURI = this.Link.SpotifyURI },
+                Image = new Image { Src = this.Image.Src, Alt = this.Image.Alt }
+            };
+        }
     }
 }
 
